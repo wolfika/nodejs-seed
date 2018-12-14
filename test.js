6bd4f7c0555ace37ae77d76b8e2488878adc42e0
Mate Farkas <mate.farkas@logmein.com>
2018-12-14 23:41:05 +0100

    pimp my nodejs-seed

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 import test from 'ava';
 import fn from '.';
 
-test('should be a function', t => {
-	t.is(typeof fn, 'function');
+test('should be a function', (t) => {
+  t.is(typeof fn, 'function');
 });
 
-test('should return Hello World', t => {
-	t.is(fn(), 'Hello World!');
+test('should return Hello World', (t) => {
+  t.is(fn(), 'Hello World!');
 });
